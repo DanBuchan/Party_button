6fbece806fa87ef0a6352f556003d3eab28da369
Daniel Buchan <clone@myself.com>
2023-11-20 12:00:22 +0000

    updated play random to control the lights

--- music_lib.py
+++ music_lib.py
@@ -3,53 +3,88 @@ import os
 import django
 import random
 import time
-import colorsys
+import multiprocessing
 
 track_path = './pb_ui/'
 sys.path.append(track_path)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'pb_ui.settings')
 django.setup()
-from mp3_manager.models import Track, Playtime, Playlist
+from mp3_manager.models import Playtime, Playlist, Bridge, Light
 
-def change_colour(pb_lights, party_light_settings, brightness, rand):
-    colours = party_light_settings
-    # step_interval = 60/bpm
-    # num_steps = playtime/step_interval
+def change_colour(pb_lights, party_light_settings, brightness, playtime, bpm):
+    # First up change the lights
+    random_light_set = []
+    for light in pb_lights:
+        if party_light_settings[light.name]:
+            if party_light_settings.random_colour:
+                random_light_set.append(light)
+            else:
+                set_light(light, party_light_settings, brightness, playtime)
 
     # we should loop over the lights, set the solid colour for the fixed ones
     # for the rest enter the infinite while loop and either make a rand change
     # or a stepped change, number of steps. For number of steps work out the
     # H and S interval and make that step each time
-    if rand:
-        while True:
-            set_lights(pb_lights, colours, brightness)
-            for i in range(len(colours)):
-                colours[i][0] = random.randint(0, 65535)
-                colours[i][1] = random.randint(0, 254)
-            time.sleep(0.50)
-    else:
-        set_lights(pb_lights, colours, brightness)
+    # initial steps per minute is the bpm
+    minute_portion=playtime/60
+    steps = bpm*minute_portion
+    step_length = playtime/steps
+    step_count = 0
+    while True:
+        for light in random_light_set:
+            if party_light_settings[light.name]['interval_size'] % step_count != 0:
+                continue
+
+            random_setting = {}
+            random_setting[light.name]['primary_h'] = random.randint(0, 65535)
+            random_setting[light.name]['primary_s'] = random.randint(0, 254)
+            #check interval size modulo first
+            if party_light_settings[light.name]['random_interval']:
+                dice_roll = random.uniform(0, 1)
+                if dice_roll == 1:
+                    set_light(light, random_setting, brightness, None)
+            else:
+                set_light(light, random_setting, brightness, None)
+        time.sleep(step_length)
+        step_count += 1
 
 def get_bridge_info():
     #we'll get this out of the party button UI later
-    brightness = 0.25
-    return "192.168.1.112", "GIRjjwwRjU7If6DYHwnzY2L6qxMOJnimt4Femjvd", int(254*brightness)
+    bridge = Bridge.objects.all().first()
+    return bridge.ip, bridge.user_id, int(254*(bridge.brightness/100))
 
 def get_light_list(b):
     light_list = []
-    for light in b.lights:
-        if "PB spot" in light.name:
-            light_list.append(light)
+    try:
+        for light in b.lights:
+            if "PB spot" in light.name:
+                light_list.append(light)
+    except Exception as e:
+        print("Couldn't get live light list: "+str(e))
     return light_list
 
 def get_initial_colours(lights):
-    initial_settings = []
-    for i, light in enumerate(lights):
-        initial_settings.append([light.hue, light.saturation, light.brightness ])
+    initial_settings = {}
+    try:
+        for light in enumerate(lights):
+            initial_settings[light.name] = [light.hue, light.saturation, light.brightness ]
+    except Exception as e:
+        print("Unable to get initial light information:" + str(e))
     return(initial_settings)
 
-def set_lights(lights, settings, brightness):
+def set_light(light, settings, brightness, playtime):
+    light.hue = settings[light.name]['primary_h']
+    light.saturation = settings[light.name]['primary_v']
+    light.brightness = brightness
+    if settings[light.name]['fade']:
+        light.transitiontime = playtime*10
+        light.hue = settings[light.name]['secondary_h']
+        light.saturation = settings[light.name]['secondary_v']
+        light.brightness = brightness
+        
+
+def reset_lights(light, settings, brightness, fade):
 
     for i, light in enumerate(lights):
         light.hue = settings[i][0]
@@ -59,15 +94,10 @@ def set_lights(lights, settings, brightness):
         else:
             light.brightness = settings[i][2]    
 
+
 def get_light_settings():
-    R = 152
-    G = 52
-    B = 235
-    hsv_values = colorsys.rgb_to_hsv(R/254, G/254, B/254)
-    h = int(65535 * hsv_values[0])
-    s = int(254 * hsv_values[1])
-    v = int(254 * hsv_values[2])
-    return [[h, s, v]]
+    lights = Light.objects.all()
+    return lights
 
 def get_playtime_obj():
     '''
@@ -133,7 +163,7 @@ def calculate_playing_coordinates(track, playtime_obj):
 
     return(play_duration, start_location)
 
-def play_music(track_qset, playtime_obj, track_path, pygame):
+def play_music(track_qset, playtime_obj, track_path, pygame, pb_lights, party_light_settings, brightness):
     play_tracks = decide_playing_set(track_qset, playtime_obj)
     for track in play_tracks:
         print(f"PLAYING: {track}")
@@ -142,8 +172,11 @@ def play_music(track_qset, playtime_obj, track_path, pygame):
         play_duration, start_location = calculate_playing_coordinates(track, playtime_obj)
         print(f"STARTING PLAYBACK AT: {start_location} secs")
         print(f"PLAYING TRACK FOR: {play_duration} secs")
+        proc = multiprocessing.Process(target=change_colour, args=(pb_lights, party_light_settings, brightness, playtime_obj.playtime_seconds, track.bpm))
+        proc.start()
         pygame.mixer.music.play(start=start_location)
         time.sleep(play_duration)
         pygame.mixer.music.stop()
+        proc.terminate()
         print(f"PLAY FINISHED: {track}")
     return()
--- play_random.py
+++ play_random.py
@@ -4,39 +4,25 @@ import django
 import pygame
 from music_lib import *
 import phue
-import multiprocessing
 
 track_path = './pb_ui/'
-sys.path.append(track_path)
-os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                      'pb_ui.settings')
-django.setup()
-from mp3_manager.models import Track, Playtime, Playlist
-
-#https://discovery.meethue.com/
-# "192.168.1.112"
-# Estimate BPM
-# https://gist.github.com/jiaaro/faa96fabd252b8552066
-# https://essentia.upf.edu/tutorial_rhythm_beatdetection.html
 
 if __name__ == '__main__':
+    # initialise things
     pygame.init()
     pygame.mixer.init()
     pygame.mixer.pre_init(44100, -16, 2, 2048)
     playtime_obj = get_playtime_obj()
     hue_bridge_ip, hue_user_id, brightness = get_bridge_info()
-    party_light_settings = get_light_settings()
     b = phue.Bridge(hue_bridge_ip, hue_user_id)
+    party_light_settings = get_light_settings()
+    
+    # Do things, i.e. after button press
     pb_lights = get_light_list(b)
     initial_light_settings = get_initial_colours(pb_lights)
-    proc = multiprocessing.Process(target=change_colour, args=(pb_lights, party_light_settings, brightness, True))
-    proc.start()
-    #get the set of track
+    #get the set of tracks
     track_qset = get_tracks(playtime_obj.playlist_selection)
     #decide what set of tracks we are playing
     if playtime_obj.lights_only == False:
-        play_music(track_qset, playtime_obj, track_path, pygame)
-    proc.terminate()
-    set_lights(pb_lights, initial_light_settings, None)
-    
-
+        play_music(track_qset, playtime_obj, track_path, pygame, pb_lights, party_light_settings, brightness)
+    reset_lights(pb_lights, initial_light_settings, None)
