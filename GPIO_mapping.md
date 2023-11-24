a5e76e883adb5eebae401062cafb2a443c9a51fd
Daniel Buchan <clone@myself.com>
2023-11-24 23:28:20 +0000

    finall channels wired up

---
+++ GPIO_mapping.md
@@ -0,0 +1,6 @@
+14 - control 1 disco ball motor
+15 - control 3 Discos 1
+18 - control 5 spots
+23 - control 7 discos 2
+
+25  - input chanel
--- party_button.py
+++ party_button.py
@@ -21,7 +21,8 @@ def signal_term_handler(sigNum, frame):
     GPIO.cleanup()
     exit()
 
-def lets_party(disco_lights_channel, spotlights_channel, discoball_channel):
+def lets_party(disco_lights_channel, disco_lights_channel_2,
+               spotlights_channel, discoball_channel):
     # Initialise lights:
     playtime_obj = get_playtime_obj()
     hue_bridge_ip, hue_user_id, brightness = get_bridge_info()
@@ -44,10 +45,12 @@ def lets_party(disco_lights_channel, spotlights_channel, discoball_channel):
     #possibly there should be some brief pauses before toggling things "on"
     if playtime_obj.music_only == False:
         print("DISCO BALL: on")
-        #GPIO.output(disco_motor_channel, GPIO.HIGH)
-        #GPIO.output(disco_spots_channel, GPIO.HIGH)
+        GPIO.output(discoball_channel, GPIO.HIGH)
+        print("SPOTLIGHTS: on")
+        GPIO.output(spotlights_channel, GPIO.HIGH)
         print("DISCO LIGHT: on")
         GPIO.output(disco_lights_channel, GPIO.HIGH)
+        GPIO.output(disco_lights_channel_2, GPIO.HIGH)
     
     if playtime_obj.lights_only == False:
         # get the set of track
@@ -61,20 +64,24 @@ def lets_party(disco_lights_channel, spotlights_channel, discoball_channel):
     reset_lights(pb_lights, initial_light_settings)
     # do we need a bit of a pause between these?
     GPIO.output(disco_lights_channel, GPIO.LOW)
-    # GPIO.output(disco_lights_channel2, GPIO.LOW)
-    #GPIO.output(disco_motor_channel, GPIO.LOW)
-    #GPIO.output(disco_spots_channel, GPIO.LOW)
+    GPIO.output(disco_lights_channel_2, GPIO.LOW)
+    GPIO.output(discoball_channel, GPIO.LOW)
+    GPIO.output(spotlights_channel, GPIO.LOW)
     return(0)
 
 if __name__ == '__main__':
     GPIO.setmode(GPIO.BCM)
-    input_channel = 17
-    disco_lights_channel = 10
-    disco_lights_channel_2 = None
-    spotlights_channel = None
-    discoball_channel = None
+    input_channel = 25
+    disco_lights_channel = 15
+    disco_lights_channel_2 = 23
+    spotlights_channel = 18
+    discoball_channel = 14
     GPIO.setup(input_channel, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.setup(disco_lights_channel, GPIO.OUT)
+    GPIO.setup(disco_lights_channel_2, GPIO.OUT)
+    GPIO.setup(spotlights_channel, GPIO.OUT)
+    GPIO.setup(discoball_channel, GPIO.OUT)
+    
     
     toggle = 0
     pygame.init()
@@ -88,6 +95,6 @@ if __name__ == '__main__':
                 print("BUTTON: released\n")
             toggle=1
         else:
-            toggle = lets_party(disco_lights_channel, spotlights_channel,
-                       discoball_channel)
+            toggle = lets_party(disco_lights_channel, disco_lights_channel_2,
+                                spotlights_channel, discoball_channel)
             GPIO.setup(input_channel, GPIO.IN, pull_up_down=GPIO.PUD_UP)
