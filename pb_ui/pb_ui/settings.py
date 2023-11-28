4b27e2947243e78ea99db322671add36f7a64543
Daniel Buchan <clone@myself.com>
2023-11-28 13:08:31 +0000

    add host

--- pb_ui/pb_ui/settings.py
+++ pb_ui/pb_ui/settings.py
@@ -27,7 +27,8 @@ DEBUG = True
 ALLOWED_HOSTS = ['192.168.1.120',
                  'localhost',
                  '127.0.0.1',
-                 '192.168.1.218',]
+                 '192.168.1.218',
+                 '192.168.0.233']
 
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 # Application definition
