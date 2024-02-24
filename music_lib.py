76cd7d9094e883432cbee3cd9dd27e8f6e138f5d
Daniel Buchan <clone@myself.com>
2024-02-24 17:25:02 +0000

    getting off to work

--- music_lib.py
+++ music_lib.py
@@ -30,10 +30,9 @@ def prep_scene_data(scenes, scene_name, light_info, control_type, brightness,
             scene_id=id
     light_data = {}
     light_list = []
-    light_on = True
     for light in light_info:
         light_brightness = brightness
-        print(light, light.off)
+        light_on = True
         if light.off:
             light_on = False
         if light.override_brightness:
